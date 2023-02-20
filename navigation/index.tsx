8f0ac5c4da763a536f052f9b97e21ab4c826ebf7
filippos-kozanitis <it1@konstolymp.gr>
2023-02-20 16:24:24 +0200

    refactored into TSX

---
+++ navigation/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import LoginScreen from '../screens/Login';
+import MainMenu from '../screens/MainMenu';
+import TransferPosting from '../screens/TransferPosting';
+import TransferPostingLog from '../screens/TransferPostingLog';
+import TransferPostingHistory from '../screens/TransferPostingHistory';
+import Picking from '../screens/Picking';
+import GoodsIssues from '../screens/GoodsIssues';
+import StorageLocationList from '../screens/StorageLocationList';
+
+import {RootStackParamList} from './types';
+
+const AppStack = createNativeStackNavigator<RootStackParamList>();
+
+function AppNavigation(): JSX.Element {
+  return (
+    <AppStack.Navigator
+      initialRouteName="Login"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <AppStack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{title: 'Login'}}
+      />
+      <AppStack.Screen
+        name="MainMenu"
+        component={MainMenu}
+        options={{title: 'MainMenu'}}
+      />
+      <AppStack.Screen
+        name="TransferPosting"
+        component={TransferPosting}
+        options={{title: 'TransferPosting'}}
+      />
+      <AppStack.Screen
+        name="TransferPostingLog"
+        component={TransferPostingLog}
+        options={{title: 'TransferPostingLog'}}
+      />
+      <AppStack.Screen
+        name="TransferPostingHistory"
+        component={TransferPostingHistory}
+        options={{title: 'TransferPostingHistory'}}
+      />
+      <AppStack.Screen
+        name="Picking"
+        component={Picking}
+        options={{title: 'Picking'}}
+      />
+      <AppStack.Screen
+        name="GoodsIssues"
+        component={GoodsIssues}
+        options={{title: 'GoodsIssues'}}
+      />
+      <AppStack.Screen
+        name="StorageLocationList"
+        component={StorageLocationList}
+        options={{title: 'StorageLocationList'}}
+      />
+    </AppStack.Navigator>
+  );
+}
+
+export default AppNavigation;
---
+++ navigation/types.tsx
@@ -0,0 +1,12 @@
+export type RootStackParamList = {
+  Login: undefined;
+  MainMenu: undefined;
+  TransferPosting: undefined;
+  TransferPostingLog: undefined;
+  TransferPostingHistory: undefined;
+  Picking: undefined;
+  GoodsIssues: undefined;
+  StorageLocationList: undefined;
+};
+
+//TODO: add types
