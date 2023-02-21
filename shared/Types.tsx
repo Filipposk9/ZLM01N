9716726a8e60eb9d9c76024dc926711d9da20431
filippos-kozanitis <it1@konstolymp.gr>
2023-02-21 14:20:57 +0200

    Data type interfaces

--- shared/Types.tsx
+++ shared/Types.tsx
@@ -1,15 +1,15 @@
 export interface Material {
-  materialNumber: number;
+  materialNumber: string;
   description: string;
   materialGroup: string;
   batch: string;
   quantity: number;
   unitOfMeasure: string;
-  plant: number;
+  plant: string;
   storageLocation: StorageLocation;
 }
 
 export interface StorageLocation {
-  storageLocation: number;
+  storageLocation: string;
   description: string;
 }
