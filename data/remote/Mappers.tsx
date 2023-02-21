40ae220b66f8f46a96cdb8ab676fe4d459e616a5
filippos-kozanitis <it1@konstolymp.gr>
2023-02-21 12:41:17 +0200

    mapping routines for server responses

---
+++ data/remote/Mappers.tsx
@@ -0,0 +1,27 @@
+import {Material, StorageLocation} from '../../shared/Types';
+import {MaterialModel} from './model/MaterialModel';
+import {StorageLocationModel} from './model/StorageLocationModel';
+
+export const materialModelToMaterial = (
+  materialModel: MaterialModel,
+): Material => {
+  return Object.freeze({
+    materialNumber: materialModel.materialNumber,
+    description: materialModel.description,
+    materialGroup: materialModel.materialGroup,
+    batch: materialModel.batch,
+    quantity: materialModel.quantity,
+    unitOfMeasure: materialModel.unitOfMeasure,
+    plant: materialModel.plant,
+    storageLocation: materialModel.storageLocation,
+  } as const);
+};
+
+export const storageLocationModelToStorageLocation = (
+  storageLocationModel: StorageLocationModel,
+): StorageLocation => {
+  return Object.freeze({
+    storageLocation: storageLocationModel.storageLocation,
+    description: storageLocationModel.description,
+  });
+};
