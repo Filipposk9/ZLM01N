1829b5605116b259f74a9a554d5bfdbdf1b92173
filippos-kozanitis <it1@konstolymp.gr>
2023-02-22 01:08:11 +0200

    added spinner, dark mode switch

--- screens/login/index.tsx
+++ screens/login/index.tsx
@@ -2,30 +2,39 @@ import React, {useState, useContext} from 'react';
 import {View} from 'react-native';
 import {styles} from '../../styles/LoginStyles.js';
 
-import LoadingIndicator from '../../components/LoadingIndicator';
+import DarkModeSwitch from '../../components/DarkModeSwitch.js';
+import {HorizontalRotation} from '../../animations/HorizontalRotation.js';
+
+import Spinner from 'react-native-loading-spinner-overlay';
 import {ThemeContext} from '../../styles/ThemeContext.js';
 
 //TODO: Logout and clear()
 
-const {theme, toggle} = useContext(ThemeContext);
+const {theme} = useContext(ThemeContext);
 
 function Login({navigation}: {navigation: any}): JSX.Element {
   const [username, setUsername] = useState('FILKOZ');
   const [password, setPassword] = useState('COMPO2SITION4');
 
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <LoginContainer>
-      <LoadingIndicator />
-    </LoginContainer>
+    <View style={styles(theme).loginContainer}>
+      <Spinner
+        visible={isLoading}
+        textContent={'Σύνδεση...'}
+        textStyle={{color: 'white'}}
+      />
+      <View style={styles(theme).darkModeSwitchContainer}>
+        <DarkModeSwitch
+          icon={'feather'}
+          color={'red'}
+          size={30}
+          animation={HorizontalRotation}
+        />
+      </View>
+    </View>
   );
 }
-
-interface LoginContainerProps {
-  children?: React.ReactNode;
-}
-
-const LoginContainer: React.FC<LoginContainerProps> = ({children}) => {
-  return <View style={styles(theme).loginContainer}>{children}</View>;
-};
 
 export default React.memo(Login);
