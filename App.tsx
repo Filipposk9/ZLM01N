c127f7bdb1695c27a0a7c34067340598879bf36f
filippos-kozanitis <it1@konstolymp.gr>
2023-02-22 16:33:17 +0200

    removed unused improts

--- App.tsx
+++ App.tsx
@@ -9,19 +9,15 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {withAppStore} from './redux/Store';
 import {ThemeProvider} from './styles/ThemeContext';
-//import UserContext from './realm/DBSchema';
-//const {RealmProvider} = UserContext;
 
 import AppNavigation from './navigation';
 
 function AppStack(): JSX.Element {
   return (
     <ThemeProvider>
-      {/*<RealmProvider>*/}
       <NavigationContainer>
         <AppNavigation />
       </NavigationContainer>
-      {/*</RealmProvider>*/}
     </ThemeProvider>
   );
 }
