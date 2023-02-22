e4267fa0194be2b35effebcdf5997cea18f3093f
filippos-kozanitis <it1@konstolymp.gr>
2023-02-22 09:27:47 +0200

    initial route new login screen

--- screens/login/index.tsx
+++ screens/login/index.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useContext} from 'react';
-import {View} from 'react-native';
+import {View, TextInput} from 'react-native';
 import {styles} from '../../styles/LoginStyles.js';
 
 import DarkModeSwitch from '../../components/DarkModeSwitch.js';
@@ -33,6 +33,50 @@ function Login({navigation}: {navigation: any}): JSX.Element {
           animation={HorizontalRotation}
         />
       </View>
+      <TextInput
+        style={styles(theme).usernameInput}
+        placeholder="Username"
+        placeholderTextColor="white"
+        onChangeText={username => setUsername(username)}
+        value={username}
+      />
+      <TextInput
+        style={styles(theme).passwordInput}
+        placeholder="Password"
+        placeholderTextColor="white"
+        secureTextEntry={true}
+        onChangeText={password => setPassword(password)}
+        value={password}
+      />
+
+      {/* <Pressable
+              style={styles(theme).loginBtn}
+              onPress={() => {
+                setLoading(true);
+
+                let usernameChecks = collection.some(element => {
+                  if (element.username === username) {
+                    if (element.password === password) {
+                      return true;
+                    } else {
+                      return false;
+                    }
+                  } else {
+                    return false;
+                  }
+                });
+
+                setLoading(false);
+
+                if (usernameChecks) {
+                  navigation.navigate('MainMenu');
+                } else {
+                  alert('Λάθος όνομα/κωδικός χρήστη');
+                }
+              }}
+              android_ripple={GlobalStyles(theme).rippleColor}>
+              <Text style={styles(theme).loginBtnText}>Login</Text>
+            </Pressable> */}
     </View>
   );
 }
