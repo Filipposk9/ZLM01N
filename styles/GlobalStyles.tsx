e42346ff66c426a763ed7a446c46487110f0ad11
filippos-kozanitis <it1@konstolymp.gr>
2023-02-22 15:02:52 +0200

    refactored to tsx

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 
-export const GlobalStyles = theme =>
+export const GlobalStyles = (theme: any) =>
   StyleSheet.create({
     rippleColor: {color: theme.rippleColor, borderless: true},
   });
