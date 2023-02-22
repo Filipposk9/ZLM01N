b799f9ddc824b95d6179ca79b850c45f1271aa37
filippos-kozanitis <it1@konstolymp.gr>
2023-02-22 12:21:25 +0200

    getUser

---
+++ data/local/dao/UserDao.tsx
@@ -0,0 +1,23 @@
+import {UserSchema} from '../schema/UserSchema';
+import {User} from '../../../shared/Types';
+import {SCHEMA_NAME} from '../SchemaName';
+import BaseDao from './BaseDao';
+
+class UserDao {
+  async getUser(username: string) {
+    const user = await BaseDao.getCopyObjectById<typeof UserSchema>(
+      SCHEMA_NAME.USER,
+      username,
+    );
+    if (!user) {
+      return undefined;
+    }
+    return user;
+  }
+
+  async getUsers(): Promise<User[]> {
+    return await BaseDao.getAllObjects<User>(SCHEMA_NAME.USER);
+  }
+}
+
+export default new UserDao();
