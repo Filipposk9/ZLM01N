8fc8dbea881a75bab8da1dabc46a365deba17874
filippos-kozanitis <it1@konstolymp.gr>
2023-02-24 13:39:37 +0200

    refactored to tsx

--- credentials/CredentialStorage.tsx
+++ credentials/CredentialStorage.tsx
@@ -1,7 +1,11 @@
 import EncryptedStorage from 'react-native-encrypted-storage';
 
 class CredentialStorage {
-  static async setCredentials(username, password, csrfToken) {
+  static async setCredentials(
+    username: string,
+    password: string,
+    csrfToken: string,
+  ) {
     await EncryptedStorage.setItem(
       'user_session',
       JSON.stringify({
@@ -12,7 +16,7 @@ class CredentialStorage {
     );
   }
 
-  static async getCredentials() {
+  static async getCredentials(): Promise<string | null> {
     return await EncryptedStorage.getItem('user_session');
   }
 }
