e34bc36fdd0cc593bea160a119efbdaf154f4e29
filippos-kozanitis <it1@konstolymp.gr>
2023-02-26 16:05:10 +0200

    new data type for material docs

---
+++ data/remote/model/MaterialDocumentModel.tsx
@@ -0,0 +1,20 @@
+export interface MaterialDocumentModel {
+  MATERIALDOCUMENTNUMBER: string;
+  ITEMS: MaterialDocumentItem[];
+}
+
+export interface MaterialDocumentResponse {
+  data: MaterialDocumentModel;
+}
+
+interface MaterialDocumentItem {
+  COUNT: number;
+  MATERIALNUMBER: string;
+  BATCH: string;
+  QUANTITY: number;
+  UNITOFMEASURE: string;
+  STORAGELOCATIONIN: string;
+  STORAGELOCATIONOUT: string;
+  MOVEMENTTYPE: string;
+  PRODUCTIONORDER: string;
+}
