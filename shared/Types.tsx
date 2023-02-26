340010b5380a4113b1fc5a422d2344fe8c6f02e4
filippos-kozanitis <it1@konstolymp.gr>
2023-02-26 13:30:54 +0200

    added material document & label interfaces

--- shared/Types.tsx
+++ shared/Types.tsx
@@ -14,3 +14,26 @@ export interface StorageLocation {
   storageLocation: string;
   description: string;
 }
+
+export interface MaterialDocument {
+  goodsMovementCode: string;
+  items: MaterialDocumentItem[];
+}
+
+export interface MaterialDocumentItem {
+  count: number;
+  materialNumber: string;
+  batch: string;
+  quantity: number;
+  storageLocationIn: string;
+  storageLocationOut: string;
+  movementType: string;
+  productionOrder: string;
+}
+
+export interface Label {
+  count: number;
+  materialNumber: string;
+  batch: string;
+  quantity: number;
+}
