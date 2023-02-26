d04f15e3d047eda303c20822beb23f91f738c3b4
filippos-kozanitis <it1@konstolymp.gr>
2023-02-26 16:04:05 +0200

    materialDocument & label mapper

--- data/remote/Mappers.tsx
+++ data/remote/Mappers.tsx
@@ -1,6 +1,15 @@
-import {Material, StorageLocation} from '../../shared/Types';
+import {
+  Material,
+  StorageLocation,
+  GoodsMovement,
+  GoodsMovementItem,
+  MaterialDocument,
+  MaterialDocumentItem,
+  Label,
+} from '../../shared/Types';
 import {MaterialModel} from './model/MaterialModel';
 import {StorageLocationModel} from './model/StorageLocationModel';
+import {MaterialDocumentModel} from './model/MaterialDocumentModel';
 
 export const materialModelToMaterial = (
   materialModel: MaterialModel,
@@ -19,5 +28,56 @@ export const storageLocationModelToStorageLocation = (
   return Object.freeze({
     storageLocation: storageLocationModel.STORAGELOCATION,
     description: storageLocationModel.DESCRIPTION,
+  });
+};
+
+export const materialDocumentModelToMaterialDocument = (
+  materialDocumentModel: MaterialDocumentModel,
+): MaterialDocument => {
+  const items: MaterialDocumentItem[] = materialDocumentModel.ITEMS.map(
+    label => {
+      return Object.freeze({
+        count: label.COUNT,
+        materialNumber: label.MATERIALNUMBER,
+        batch: label.BATCH,
+        quantity: label.QUANTITY,
+        storageLocationIn: label.STORAGELOCATIONIN,
+        storageLocationOut: label.STORAGELOCATIONOUT,
+        movementType: label.MOVEMENTTYPE,
+        productionOrder: label.PRODUCTIONORDER,
+      });
+    },
+  );
+
+  return Object.freeze({
+    materialDocumentNumber: materialDocumentModel.MATERIALDOCUMENTNUMBER,
+    items: items,
+  });
+};
+
+export const labelToGoodsMovement = (
+  goodsMovementCode: string,
+  scannedLabels: Label[],
+  storageLocationIn: string,
+  storageLocationOut: string,
+  movementType: string,
+  productionOrder: string,
+): GoodsMovement => {
+  const items: GoodsMovementItem[] = scannedLabels.map(label => {
+    return Object.freeze({
+      count: label.count,
+      materialNumber: label.materialNumber,
+      batch: label.batch,
+      quantity: label.quantity,
+      storageLocationIn: storageLocationIn,
+      storageLocationOut: storageLocationOut,
+      movementType: movementType,
+      productionOrder: productionOrder,
+    });
+  });
+
+  return Object.freeze({
+    goodsMovementCode: goodsMovementCode,
+    items: items,
   });
 };
