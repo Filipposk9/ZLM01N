d015e3fb4b48fd34835f695ba9c0a08f6b65300f
filippos-kozanitis <it1@konstolymp.gr>
2023-03-01 10:05:08 +0200

    new picking screen initial commit

--- src/screens/picking/index.tsx
+++ src/screens/picking/index.tsx
@@ -6,6 +6,7 @@ import {
   TextInput,
   Pressable,
   Alert,
+  FlatList,
 } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
 import {styles} from '../../styles/PickingStyles';
@@ -15,7 +16,6 @@ import BarcodeScanner from '../../components/BarcodeScanner';
 import Repository from '../../data/Repository';
 import {OutboundDelivery} from '../../shared/Types';
 
-//TODO: props: pickingProps
 function Picking({navigation}: {navigation: any}): JSX.Element {
   const {theme} = useContext(ThemeContext);
 
@@ -25,10 +25,10 @@ function Picking({navigation}: {navigation: any}): JSX.Element {
     OutboundDelivery | undefined
   >();
 
-  const validateOutboundDelivery = (deliveryNr: string) => {
+  const validateOutboundDelivery = (outboundDeliveryNumber: string) => {
     let outboundDeliveryRegex = new RegExp('^80[0-9]{6}$');
 
-    return outboundDeliveryRegex.test(deliveryNr);
+    return outboundDeliveryRegex.test(outboundDeliveryNumber);
   };
 
   return (
@@ -52,20 +52,17 @@ function Picking({navigation}: {navigation: any}): JSX.Element {
             }
             onSubmitEditing={async () => {
               if (validateOutboundDelivery(outboundDelivery)) {
-                const response: OutboundDelivery | undefined =
-                  await Repository.getOutboundDelivery(outboundDelivery);
+                const response = await Repository.getOutboundDelivery(
+                  outboundDelivery,
+                );
 
                 if (response !== undefined) {
                   response.items.sort(
                     (a, b) => a.positionNumber - b.positionNumber,
                   );
+
+                  setOutboundDeliveryData(response);
                 }
-
-                setOutboundDeliveryData(response);
-
-                console.log(response.items[0]);
-
-                //setOutboundDeliveryKey(outboundDeliveryKey + 1);
               } else {
                 Alert.alert('Λάθος αριθμός παράδοσης');
               }
@@ -74,67 +71,133 @@ function Picking({navigation}: {navigation: any}): JSX.Element {
         </View>
       </View>
 
-      {/* <View>
-        {outboundDeliveryData !== undefined && outboundDeliveryData !== null ? (
-          <OutboundDelivery
-            key={outboundDeliveryKey}
-            ref={outboundDeliveryRef}
-            data={outboundDeliveryData}
-          />
-        ) : null}
-      </View> */}
+      <BarcodeScanner
+        onScan={async invisibleText => {
+          let response;
 
-      <View style={styles(theme).invisibleInputContainer}>
-        {/* Main Body uses an invisible text input to pick a handling unit as soon as its scanned on this screen*/}
-        <BarcodeScanner
-          onScan={async invisibleText => {
-            let response;
+          //   if (outboundDeliveryRef.current.outboundDeliveryData?.header.pickingStatus === 'C') {
+          //     alert('Το picking έχει ήδη ολοκληρωθεί');
+          //   } else {
+          //     try {
+          //       setLoading(true);
+          //       response = await SapRequestHandler.createPickingRequest(
+          //         invisibleText,
+          //         outboundDelivery,
+          //       );
+          //       setLoading(false);
+          //     } catch (error) {
+          //       console.log(error);
+          //     }
+          //   }
+          //   {
+          //     /* refresh outbound delivery contents */
+          //   }
+          //   if (response[0].CODE === 0) {
+          //     console.log(response);
 
-            //   if (outboundDeliveryRef.current.header.pickingStatus === 'C') {
-            //     alert('Το picking έχει ήδη ολοκληρωθεί');
-            //   } else {
-            //     try {
-            //       setLoading(true);
-            //       response = await SapRequestHandler.createPickingRequest(
-            //         invisibleText,
-            //         outboundDelivery,
-            //       );
-            //       setLoading(false);
-            //     } catch (error) {
-            //       console.log(error);
-            //     }
-            //   }
-            //   {
-            //     /* refresh outbound delivery contents */
-            //   }
-            //   if (response[0].CODE === 0) {
-            //     console.log(response);
+          //     if (validateOutboundDelivery(outboundDelivery)) {
+          //       let response = await SapRequestHandler.getOutboundDeliveryData(
+          //         outboundDelivery,
+          //       );
 
-            //     if (validateOutboundDelivery(outboundDelivery)) {
-            //       let response = await SapRequestHandler.getOutboundDeliveryData(
-            //         outboundDelivery,
-            //       );
+          //       response.ITEMS.sort((a, b) => a.POSNR - b.POSNR);
 
-            //       response.ITEMS.sort((a, b) => a.POSNR - b.POSNR);
+          //       setOutboundDeliveryData(response);
+          //       setOutboundDeliveryKey(outboundDeliveryKey + 1);
+          //     } else {
+          //       alert('Λάθος αριθμός παράδοσης');
+          //     }
+          //   } else {
+          //     console.log(response[0]);
+          //     console.log('Picking failed');
+          //   }
+          //   {
+          //     if (invisibleInputRef.current) {
+          //       invisibleInputRef.current.focus();
+          //     }
+          //   }
+          //   setInvisibleText('');
+          // }}
+        }}
+      />
 
-            //       setOutboundDeliveryData(response);
-            //       setOutboundDeliveryKey(outboundDeliveryKey + 1);
-            //     } else {
-            //       alert('Λάθος αριθμός παράδοσης');
-            //     }
-            //   } else {
-            //     console.log(response[0]);
-            //     console.log('Picking failed');
-            //   }
-            //   {
-            //     if (invisibleInputRef.current) {
-            //       invisibleInputRef.current.focus();
-            //     }
-            //   }
-            //   setInvisibleText('');
-            // }}
-          }}
-        />
+      <View style={styles(theme).outboundDeliveryContainer}>
+        <Text style={styles(theme).outboundDeliveryHeaderText}>
+          Πελάτης: {outboundDeliveryData?.header.customerName}
+        </Text>
+        <Text style={styles(theme).outboundDeliveryHeaderText}>
+          Προορισμός: {outboundDeliveryData?.header.shipToPartyName}
+        </Text>
+        <Text
+          style={[
+            styles(theme).outboundDeliveryHeaderText,
+            outboundDeliveryData?.header.status === 'C'
+              ? {color: 'red'}
+              : {color: theme.buttonTextColor},
+          ]}>
+          Picking Status: {outboundDeliveryData?.header.status}
+        </Text>
+
+        <View style={styles(theme).outboundDeliveryLinesContainer}>
+          <FlatList
+            data={outboundDeliveryData?.items}
+            renderItem={({item, index}) => (
+              <View>
+                {/* <Pressable onPress={onChangeLayout.bind(this, index)}> */}
+                <Text style={styles(theme).outboundDeliveryLinesText}>
+                  {item.positionNumber}. {item.materialText}
+                </Text>
+                <Text style={styles(theme).outboundDeliveryLinesText}>
+                  Picked: {item.pickedQuantity}/{item.requirementQuantity}
+                  {item.unitOfMeasure}
+                </Text>
+                <Text style={styles(theme).outboundDeliveryLinesText}>
+                  Scanned: {0} PAL
+                </Text>
+                {/* </Pressable> */}
+                <View
+                // style={{
+                //   height: state.expanded[index] ? null : 0,
+                //   overflow: 'hidden',
+                // }}
+                >
+                  <FlatList
+                    data={item.handlingUnits}
+                    renderItem={({item}) => (
+                      <View
+                        style={
+                          styles(theme).outboundDeliveryHandlingUnitsContainer
+                        }>
+                        <Text
+                          style={
+                            styles(theme).outboundDeliveryHandlingUnitsText
+                          }>
+                          SSCC: {item.sscc}
+                        </Text>
+                        <Text
+                          style={
+                            styles(theme).outboundDeliveryHandlingUnitsText
+                          }>
+                          Παρτίδα: {item.batch}
+                        </Text>
+                        <Text
+                          style={
+                            styles(theme).outboundDeliveryHandlingUnitsText
+                          }>
+                          Ποσότητα: {item.quantity} {item.unitOfMeasure}
+                        </Text>
+                        <Text
+                          style={
+                            styles(theme).outboundDeliveryHandlingUnitsText
+                          }>
+                          Αποθ. Χώρος: {item.storageLocation}
+                        </Text>
+                      </View>
+                    )}></FlatList>
+                </View>
+              </View>
+            )}></FlatList>
+        </View>
       </View>
     </View>
   );
