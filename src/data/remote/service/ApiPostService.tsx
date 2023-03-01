5301c788557c9950f389105f34b97ec1ccc62df0
filippos-kozanitis <it1@konstolymp.gr>
2023-03-01 15:10:37 +0200

    added picking request method

--- src/data/remote/service/ApiPostService.tsx
+++ src/data/remote/service/ApiPostService.tsx
@@ -1,9 +1,18 @@
-import {Label, MaterialDocument, GoodsMovement} from '../../../shared/Types';
 import {
+  Label,
+  MaterialDocument,
+  GoodsMovement,
+  PickingRequest,
+  Picking,
+} from '../../../shared/Types';
+import {
+  handlingUnitToPickingRequest,
   labelToGoodsMovement,
   materialDocumentModelToMaterialDocument,
+  pickingModelToPicking,
 } from '../Mappers';
 import {MaterialDocumentResponse} from '../model/MaterialDocumentModel';
+import {PickingResponse} from '../model/PickingModel';
 import RequestGateway, {isError} from '../RequestGateway';
 import SapRequestParameters from '../SapRequestParameters';
 
@@ -39,6 +48,30 @@ class ApiPostService {
       return undefined;
     } else {
       return materialDocumentModelToMaterialDocument(response.result.data);
+    }
+  }
+
+  async createPickingRequest(
+    outboundDeliveryNumber: string,
+    sscc: string,
+  ): Promise<Picking | undefined> {
+    const pickingRequest: PickingRequest = handlingUnitToPickingRequest(
+      outboundDeliveryNumber,
+      sscc,
+    );
+
+    const sapRequestHeaders = await SapRequestParameters.getSapRequestHeaders();
+
+    const response = await RequestGateway.post<PickingResponse>(
+      '/picking',
+      sapRequestHeaders,
+      pickingRequest,
+    );
+
+    if (isError(response)) {
+      return undefined;
+    } else {
+      return pickingModelToPicking(response.result.data);
     }
   }
 }
