258ee5ce0e7e5e2cd10423cc27fbaca6d715841f
filippos-kozanitis <it1@konstolymp.gr>
2023-03-01 15:10:01 +0200

    picking screen working version

--- src/screens/picking/index.tsx
+++ src/screens/picking/index.tsx
@@ -1,16 +1,7 @@
-import React, {useContext, useState} from 'react';
-import {
-  ScrollView,
-  View,
-  Text,
-  TextInput,
-  Pressable,
-  Alert,
-  FlatList,
-} from 'react-native';
+import React, {useContext, useState, useRef} from 'react';
+import {View, Text, TextInput, Pressable, Alert, FlatList} from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
 import {styles} from '../../styles/PickingStyles';
-import {GlobalStyles} from '../../styles/GlobalStyles';
 import {ThemeContext} from '../../styles/ThemeContext';
 import BarcodeScanner from '../../components/BarcodeScanner';
 import Repository from '../../data/Repository';
@@ -24,12 +15,111 @@ function Picking({navigation}: {navigation: any}): JSX.Element {
   const [outboundDeliveryData, setOutboundDeliveryData] = useState<
     OutboundDelivery | undefined
   >();
+  const [expanded, setExpanded] = useState<boolean[]>([]);
+
+  const inputRef = useRef<TextInput>(null);
 
   const validateOutboundDelivery = (outboundDeliveryNumber: string) => {
     let outboundDeliveryRegex = new RegExp('^80[0-9]{6}$');
 
     return outboundDeliveryRegex.test(outboundDeliveryNumber);
   };
+
+  const onChangeLayout = (index: number) => {
+    const nextState = expanded.map((c, i) => {
+      if (i === index) {
+        if (c) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    });
+
+    if (nextState !== undefined) {
+      setExpanded(nextState);
+    }
+  };
+
+  const getOutboundDeliveryData = () => {
+    const getOutboundDeliveryData = async () => {
+      if (validateOutboundDelivery(outboundDelivery)) {
+        const response = await Repository.getOutboundDelivery(outboundDelivery);
+
+        if (response !== undefined) {
+          response.items.sort((a, b) => a.positionNumber - b.positionNumber);
+
+          setOutboundDeliveryData(response);
+
+          const initialState = new Array(response.items.length).fill(false);
+
+          setExpanded(initialState);
+        }
+      } else {
+        Alert.alert('Λάθος αριθμός παράδοσης');
+
+        if (inputRef.current) {
+          inputRef.current.focus();
+        }
+      }
+    };
+
+    getOutboundDeliveryData();
+  };
+
+  const pickLabel = (lastScannedBarcode: string) => {
+    console.log(lastScannedBarcode);
+
+    const pickLabel = async () => {
+      if (lastScannedBarcode !== '') {
+        if (outboundDeliveryData?.header.status === 'C') {
+          Alert.alert('To picking έχει ήδη ολοκληρωθεί');
+
+          if (inputRef.current) {
+            inputRef.current.focus();
+          }
+        } else {
+          setLoading(true);
+
+          const response = await Repository.createPickingRequest(
+            outboundDelivery,
+            lastScannedBarcode,
+          );
+
+          if (response !== undefined) {
+            if (response.code === 0) {
+              if (validateOutboundDelivery(outboundDelivery)) {
+                const refreshedOutboundDelivery =
+                  await Repository.getOutboundDelivery(outboundDelivery);
+                if (refreshedOutboundDelivery !== undefined) {
+                  refreshedOutboundDelivery.items.sort(
+                    (a, b) => a.positionNumber - b.positionNumber,
+                  );
+                  setOutboundDeliveryData(refreshedOutboundDelivery);
+                  const initialState = new Array(
+                    refreshedOutboundDelivery.items.length,
+                  ).fill(false);
+                  setExpanded(initialState);
+                }
+              }
+            } else {
+              Alert.alert(response.message);
+
+              if (inputRef.current) {
+                inputRef.current.focus();
+              }
+            }
+          }
+
+          setLoading(false);
+        }
+      }
+    };
+
+    pickLabel();
+  };
+
+  //TODO: animate flatlist spawning
 
   return (
     <View style={styles(theme).pickingContainer}>
@@ -47,79 +137,19 @@ function Picking({navigation}: {navigation: any}): JSX.Element {
           <TextInput
             style={styles(theme).outboundDeliveryInputField}
             keyboardType="number-pad"
-            onChangeText={outboundDelivery =>
-              setOutboundDelivery(outboundDelivery)
-            }
-            onSubmitEditing={async () => {
-              if (validateOutboundDelivery(outboundDelivery)) {
-                const response = await Repository.getOutboundDelivery(
-                  outboundDelivery,
-                );
+            onChangeText={outboundDelivery => {
+              setOutboundDelivery(outboundDelivery);
+            }}
+            onSubmitEditing={() => {
+              getOutboundDeliveryData();
 
-                if (response !== undefined) {
-                  response.items.sort(
-                    (a, b) => a.positionNumber - b.positionNumber,
-                  );
-
-                  setOutboundDeliveryData(response);
-                }
-              } else {
-                Alert.alert('Λάθος αριθμός παράδοσης');
+              if (inputRef.current) {
+                inputRef.current.focus();
               }
             }}
             value={outboundDelivery}></TextInput>
         </View>
       </View>
-
-      <BarcodeScanner
-        onScan={async invisibleText => {
-          let response;
-
-          //   if (outboundDeliveryRef.current.outboundDeliveryData?.header.pickingStatus === 'C') {
-          //     alert('Το picking έχει ήδη ολοκληρωθεί');
-          //   } else {
-          //     try {
-          //       setLoading(true);
-          //       response = await SapRequestHandler.createPickingRequest(
-          //         invisibleText,
-          //         outboundDelivery,
-          //       );
-          //       setLoading(false);
-          //     } catch (error) {
-          //       console.log(error);
-          //     }
-          //   }
-          //   {
-          //     /* refresh outbound delivery contents */
-          //   }
-          //   if (response[0].CODE === 0) {
-          //     console.log(response);
-
-          //     if (validateOutboundDelivery(outboundDelivery)) {
-          //       let response = await SapRequestHandler.getOutboundDeliveryData(
-          //         outboundDelivery,
-          //       );
-
-          //       response.ITEMS.sort((a, b) => a.POSNR - b.POSNR);
-
-          //       setOutboundDeliveryData(response);
-          //       setOutboundDeliveryKey(outboundDeliveryKey + 1);
-          //     } else {
-          //       alert('Λάθος αριθμός παράδοσης');
-          //     }
-          //   } else {
-          //     console.log(response[0]);
-          //     console.log('Picking failed');
-          //   }
-          //   {
-          //     if (invisibleInputRef.current) {
-          //       invisibleInputRef.current.focus();
-          //     }
-          //   }
-          //   setInvisibleText('');
-          // }}
-        }}
-      />
 
       <View style={styles(theme).outboundDeliveryContainer}>
         <Text style={styles(theme).outboundDeliveryHeaderText}>
@@ -143,62 +173,69 @@ function Picking({navigation}: {navigation: any}): JSX.Element {
             data={outboundDeliveryData?.items}
             renderItem={({item, index}) => (
               <View>
-                {/* <Pressable onPress={onChangeLayout.bind(this, index)}> */}
-                <Text style={styles(theme).outboundDeliveryLinesText}>
-                  {item.positionNumber}. {item.materialText}
-                </Text>
-                <Text style={styles(theme).outboundDeliveryLinesText}>
-                  Picked: {item.pickedQuantity}/{item.requirementQuantity}
-                  {item.unitOfMeasure}
-                </Text>
-                <Text style={styles(theme).outboundDeliveryLinesText}>
-                  Scanned: {0} PAL
-                </Text>
-                {/* </Pressable> */}
+                <Pressable onPress={() => onChangeLayout(index)}>
+                  <Text style={styles(theme).outboundDeliveryLinesText}>
+                    {item.positionNumber}. {item.materialText}
+                  </Text>
+                  <Text style={styles(theme).outboundDeliveryLinesText}>
+                    Picked: {item.pickedQuantity}/{item.requirementQuantity}
+                    {item.unitOfMeasure}
+                  </Text>
+                  <Text style={styles(theme).outboundDeliveryLinesText}>
+                    Scanned: {0} PAL
+                  </Text>
+                </Pressable>
                 <View
-                // style={{
-                //   height: state.expanded[index] ? null : 0,
-                //   overflow: 'hidden',
-                // }}
-                >
+                  style={{
+                    height: expanded[index] ? null : 0,
+                    overflow: 'hidden',
+                  }}>
                   <FlatList
                     data={item.handlingUnits}
-                    renderItem={({item}) => (
-                      <View
-                        style={
-                          styles(theme).outboundDeliveryHandlingUnitsContainer
-                        }>
-                        <Text
+                    renderItem={({item}) => {
+                      //TODO: make into 1 component
+                      return (
+                        <View
                           style={
-                            styles(theme).outboundDeliveryHandlingUnitsText
+                            styles(theme).outboundDeliveryHandlingUnitsContainer
                           }>
-                          SSCC: {item.sscc}
-                        </Text>
-                        <Text
-                          style={
-                            styles(theme).outboundDeliveryHandlingUnitsText
-                          }>
-                          Παρτίδα: {item.batch}
-                        </Text>
-                        <Text
-                          style={
-                            styles(theme).outboundDeliveryHandlingUnitsText
-                          }>
-                          Ποσότητα: {item.quantity} {item.unitOfMeasure}
-                        </Text>
-                        <Text
-                          style={
-                            styles(theme).outboundDeliveryHandlingUnitsText
-                          }>
-                          Αποθ. Χώρος: {item.storageLocation}
-                        </Text>
-                      </View>
-                    )}></FlatList>
+                          <Text
+                            style={
+                              styles(theme).outboundDeliveryHandlingUnitsText
+                            }>
+                            SSCC: {item.sscc}
+                          </Text>
+                          <Text
+                            style={
+                              styles(theme).outboundDeliveryHandlingUnitsText
+                            }>
+                            Παρτίδα: {item.batch}
+                          </Text>
+                          <Text
+                            style={
+                              styles(theme).outboundDeliveryHandlingUnitsText
+                            }>
+                            Ποσότητα: {item.quantity} {item.unitOfMeasure}
+                          </Text>
+                          <Text
+                            style={
+                              styles(theme).outboundDeliveryHandlingUnitsText
+                            }>
+                            Αποθ. Χώρος: {item.storageLocation}
+                          </Text>
+                        </View>
+                      );
+                    }}></FlatList>
                 </View>
               </View>
             )}></FlatList>
         </View>
       </View>
+
+      <BarcodeScanner
+        reference={inputRef}
+        onScan={lastScannedBarcode => pickLabel(lastScannedBarcode)}
+      />
     </View>
   );
 }
