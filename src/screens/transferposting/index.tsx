185d61d8c16e4f6d64d7bc22d2dbc9468211c9a1
filippos-kozanitis <it1@konstolymp.gr>
2023-03-03 12:02:18 +0200

    transfer posting tabs

--- src/screens/transferposting/index.tsx
+++ src/screens/transferposting/index.tsx
@@ -25,7 +25,6 @@ import {ThemeContext} from '../../styles/ThemeContext';
 import {styles} from '../../styles/TransferPostingStyles';
 import {GlobalStyles} from '../../styles/GlobalStyles';
 
-//TODO: props: TransferPostingProps
 function TransferPosting({navigation}: {navigation: any}): JSX.Element {
   const dispatcher = useAppDispatch();
 
