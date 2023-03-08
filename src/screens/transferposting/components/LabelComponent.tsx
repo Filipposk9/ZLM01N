478b09d8e8613a2a81e753e5558de887e75e45ee
filippos-kozanitis <it1@konstolymp.gr>
2023-03-08 20:44:01 +0200

    reset screen after submit

--- src/screens/transferposting/components/LabelComponent.tsx
+++ src/screens/transferposting/components/LabelComponent.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useContext, useState} from 'react';
+import React, {useEffect, useRef, useContext, useState, ReactNode} from 'react';
 import Repository from '../../../data/Repository';
 import {
   Animated,
@@ -9,10 +9,11 @@ import {
   StyleSheet,
 } from 'react-native';
 import {GestureHandlerRootView, Swipeable} from 'react-native-gesture-handler';
-import Icon from 'react-native-vector-icons/AntDesign';
 import {ThemeContext} from '../../../styles/ThemeContext';
+import HorizontalDrag from '../../../animations/HorizontalDrag';
+import FadeIn from '../../../animations/FadeIn';
 
-interface LabelProps {
+interface LabelComponentProps {
   count: number;
   barcode: string;
   validity: boolean;
@@ -20,11 +21,12 @@ interface LabelProps {
 }
 
 //TODO: add animation fadeIn()
+//TODO: check scanned barcode validity using regex
 
-function LabelComponent(props: LabelProps): JSX.Element {
+function LabelComponent(props: LabelComponentProps): JSX.Element {
   const {count, barcode, validity, onDeletePressed} = props;
   const {theme} = useContext(ThemeContext);
-  const swipeablePanel = useRef();
+  const swipeablePanel = useRef<Swipeable>(null);
 
   const [materialDescription, setMaterialDescription] = useState<
     string | undefined
@@ -42,25 +44,14 @@ function LabelComponent(props: LabelProps): JSX.Element {
     fetchMaterialBasicData();
   }, []);
 
-  //TODO: move animation to animations folder
-
-  const renderLeftActions = (
-    dragAnimatedValue: Animated.AnimatedInterpolation<number>,
-  ) => {
-    const opacity = dragAnimatedValue.interpolate({
-      inputRange: [0, 50, 100, 101],
-      outputRange: [-20, 0, 0, 1],
-      extrapolate: 'clamp',
-    });
+  const swipedPanel = (): Element => {
     return (
       <View style={styles(theme).swipedRow}>
         <Animated.View style={styles(theme).swipedContainer}>
           <Pressable
             onPress={() => {
-              if (swipeablePanel.current !== undefined) {
-                swipeablePanel.current.close();
-                onDeletePressed();
-              }
+              swipeablePanel.current?.close();
+              onDeletePressed();
             }}>
             <Text style={styles(theme).swipedText}>Διαγραφή</Text>
           </Pressable>
@@ -69,22 +60,28 @@ function LabelComponent(props: LabelProps): JSX.Element {
     );
   };
 
+  const horizontalDrag = HorizontalDrag.setInterpolate(swipedPanel);
+  const fadeIn = FadeIn.setInterpolate();
+
   return (
     <GestureHandlerRootView>
-      <Swipeable ref={swipeablePanel} renderLeftActions={renderLeftActions}>
-        <View style={styles(theme).labelItemContainer}>
+      <Swipeable ref={swipeablePanel} renderLeftActions={horizontalDrag}>
+        <Animated.View
+          style={[
+            styles(theme).labelItemContainer,
+            {
+              borderWidth: validity ? 0 : 1,
+              borderColor: validity ? '' : 'red',
+              opacity: fadeIn,
+            },
+          ]}>
           <TextInput
             style={styles(theme).labelItem}
             editable={false}
             value={count + '. ' + barcode + '\n' + materialDescription}
             multiline={true}
           />
-          <Icon
-            name={validity ? 'check' : 'close'}
-            size={30}
-            color={validity ? 'green' : 'red'}
-          />
-        </View>
+        </Animated.View>
       </Swipeable>
     </GestureHandlerRootView>
   );
@@ -94,6 +91,7 @@ const styles = (theme: any) =>
   StyleSheet.create({
     swipedRow: {
       flexDirection: 'row',
+      alignItems: 'center',
     },
     swipedContainer: {
       backgroundColor: 'red',
@@ -112,17 +110,17 @@ const styles = (theme: any) =>
       color: 'white',
     },
     labelItemContainer: {
-      borderWidth: 1,
-      borderColor: theme.borderColor,
+      elevation: 5,
       backgroundColor: theme.secondaryForegroundColor,
       borderRadius: 15,
       margin: '3%',
       alignItems: 'center',
       flexDirection: 'row',
+      flex: 1,
     },
     labelItem: {
+      fontSize: 15,
       color: theme.secondaryTextColor,
-      paddingLeft: '5%',
     },
   });
 
