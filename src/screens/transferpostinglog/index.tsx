41a37227b04a36ac7ea50dbdc91d4037507e3cac
it1@konstolymp.gr <it1@konstolymp.gr>
2023-03-20 16:59:57 +0200

    added storage location descriptions, material txt

--- src/screens/transferpostinglog/index.tsx
+++ src/screens/transferpostinglog/index.tsx
@@ -1,8 +1,13 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {ScrollView, View, Text} from 'react-native';
 import {styles} from '../../appearance/styles/TransferPostingLogStyles';
 import {ThemeContext} from '../../appearance/theme/ThemeContext';
-import {MaterialDocument, MaterialDocumentItem} from '../../shared/Types';
+import Repository from '../../data/Repository';
+import {
+  Material,
+  MaterialDocument,
+  MaterialDocumentItem,
+} from '../../shared/Types';
 
 function TransferPostingLog({
   route,
@@ -16,15 +21,51 @@ function TransferPostingLog({
   const goodsMovementLog: MaterialDocument = route.params[0];
 
   const storageLocationIn = route.params[1];
-  const storageLocationout = route.params[2];
+  const storageLocationOut = route.params[2];
 
-  //console.log(goodsMovementLog, 'log');
+  const [storageLocationInDescription, setStorageLocationInDescription] =
+    useState<string>();
+  const [storageLocationOutDescription, setStorageLocationOutDescription] =
+    useState<string>();
 
-  //TODO: get material texts
+  const [materialDescriptions, setMaterialDescriptions] = useState([]);
   //TODO: split in 2 arrays, (needs validity indicator from backend)
   //TODO: rework styling, add material texts
 
   useEffect(() => {
+    const getStorageLocationDescriptions = async () => {
+      const responseIn = await Repository.getStorageLocation(storageLocationIn);
+
+      if (responseIn) {
+        setStorageLocationInDescription(responseIn.description);
+      }
+
+      const responseOut = await Repository.getStorageLocation(
+        storageLocationOut,
+      );
+
+      if (responseOut) {
+        setStorageLocationOutDescription(responseOut.description);
+      }
+    };
+
+    const getMaterialDescriptions = async () => {
+      const materialTexts = [];
+
+      for (let i = 0; i < goodsMovementLog.items.length; i++) {
+        const response = await Repository.getMaterialBasicData(
+          goodsMovementLog.items[i].materialNumber,
+        );
+
+        materialTexts.push(response);
+      }
+
+      setMaterialDescriptions(materialTexts);
+    };
+
+    getStorageLocationDescriptions();
+    getMaterialDescriptions();
+
     // const { validMaterialDocument, invalidMaterialDocument } = items.reduce(
     //     (acc, item) => {
     //       if (item.category === "A") {
@@ -38,44 +79,38 @@ function TransferPostingLog({
     // )
   }, []);
 
+  console.log(materialDescriptions);
+
   return (
     <View style={styles(theme).transferPostingLogTopContainer}>
       <View style={styles(theme).transferPostingLogHeaderContainer}>
         <View style={styles(theme).transferPostingLogHeaderLine}>
-          <View style={{flex: 1}}>
-            <Text style={styles(theme).transferPostingLogHeaderText}>
-              Προέλευση:
-            </Text>
-          </View>
-          <View>
-            <Text style={{textAlign: 'right'}}>{storageLocationIn}</Text>
-          </View>
+          <Text style={styles(theme).transferPostingLogHeaderText}>
+            Προέλευση: {storageLocationInDescription}
+          </Text>
         </View>
         <View style={styles(theme).transferPostingLogHeaderLine}>
-          <View style={{flex: 1}}>
-            <Text style={styles(theme).transferPostingLogHeaderText}>
-              Προορισμός:
-            </Text>
-          </View>
-          <View>
-            <Text style={{textAlign: 'right'}}>{storageLocationout}</Text>
-          </View>
+          <Text style={styles(theme).transferPostingLogHeaderText}>
+            Προορισμός: {storageLocationOutDescription}
+          </Text>
         </View>
       </View>
-      {/*//TODO: contents do not scroll*/}
+
       <ScrollView>
+        <Text style={styles(theme).transferPostingLogTitle}>
+          {goodsMovementLog.materialDocumentNumber !== ''
+            ? 'Επιτυχημένες Ενδοδιακινήσεις'
+            : null}
+        </Text>
         <View
           style={[
             styles(theme).transferPostingLogContainer,
             {backgroundColor: theme.foregroundColor},
           ]}>
-          <Text style={styles(theme).transferPostingLogTitle}>
-            Επιτυχημένες ενδοδιακινήσεις
-          </Text>
-          <Text style={styles(theme).transferPostingLogTitle}>
+          <Text style={styles(theme).transferPostingLogMaterialNumber}>
             {goodsMovementLog.materialDocumentNumber !== ''
               ? 'Παραστατικό: ' + goodsMovementLog.materialDocumentNumber
-              : ''}
+              : null}
           </Text>
 
           {goodsMovementLog !== undefined &&
@@ -84,11 +119,28 @@ function TransferPostingLog({
                 (item: MaterialDocumentItem, i: number) => {
                   return (
                     <View key={i} style={styles(theme).transferPostingLogItem}>
-                      <Text style={{fontWeight: 'bold'}}>
-                        {item.count}. {item.materialNumber}
-                      </Text>
-                      <Text>Παρτίδα: {item.batch}</Text>
-                      <Text>Ποσότητα: {item.quantity}</Text>
+                      <View style={styles(theme).transferPostingLogPanelLeft}>
+                        <Text style={styles(theme).transferPostingLogTextLeft}>
+                          {item.count}
+                        </Text>
+                      </View>
+                      <View style={styles(theme).transferPostingLogPanelRight}>
+                        <Text
+                          style={{
+                            fontWeight: 'bold',
+                            color: theme.buttonTextColor,
+                          }}>
+                          Κωδικός Υλικού: {item.materialNumber} {'\n'}
+                          {materialDescriptions[i].description}
+                        </Text>
+                        <Text style={{color: theme.buttonTextColor}}>
+                          Παρτίδα: {item.batch}
+                        </Text>
+                        <Text style={{color: theme.buttonTextColor}}>
+                          Ποσότητα: {item.quantity}
+                        </Text>
+                        <Text></Text>
+                      </View>
                     </View>
                   );
                 },
