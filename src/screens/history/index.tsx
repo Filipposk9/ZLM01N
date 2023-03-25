b477d2630dd540e284688b583f796b6ccaccddbe
it1@konstolymp.gr <it1@konstolymp.gr>
2023-03-25 17:01:16 +0200

    log items are now animated

--- src/screens/history/index.tsx
+++ src/screens/history/index.tsx
@@ -1,6 +1,14 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {View, Text, FlatList, Pressable} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  Pressable,
+  Animated,
+  StyleSheet,
+} from 'react-native';
 import {useSelector} from 'react-redux';
+import Slide from '../../appearance/animations/Slide';
 import {styles} from '../../appearance/styles/HistoryStyles';
 import {ThemeContext} from '../../appearance/theme/ThemeContext';
 import Repository from '../../data/Repository';
@@ -10,11 +18,13 @@ import {MaterialDocument, MaterialDocumentItem} from '../../shared/Types';
 function History(): JSX.Element {
   const {theme} = useContext(ThemeContext);
 
-  const [expanded, setExpanded] = useState<boolean[]>([]);
-
   const goodsMovementLogs = useSelector((state: GoodsMovementLogState) =>
     state.goodsMovementLog.goodsMovementLog.slice().reverse(),
   );
+
+  const animations = goodsMovementLogs.map(() => {
+    return new Slide();
+  });
 
   type HashedDescriptions = {[key: string]: string};
 
@@ -66,104 +76,106 @@ function History(): JSX.Element {
     getStorageLocations();
   }, []);
 
-  const onChangeLayout = (index: number) => {
-    const nextState: boolean[] = expanded.map((c, i) => {
-      if (i === index) {
-        if (c) {
-          return false;
-        } else {
-          return true;
-        }
-      } else {
-        return null as unknown as boolean;
-      }
-    });
-
-    if (nextState !== undefined) {
-      setExpanded(nextState);
-    }
-  };
-
   return (
     <View style={styles(theme).historyContainer}>
       <View style={styles(theme).historyHeader}>
         <Text style={styles(theme).historyHeaderText}>Ιστορικό Κινήσεων</Text>
       </View>
       <FlatList
+        //TODO: make this view into a component GoodsMovemementLogComponent
         data={goodsMovementLogs}
-        renderItem={({item, index}) => (
-          <Pressable
-            onPress={() => {
-              onChangeLayout(index);
-            }}>
-            <View
-              style={[
-                styles(theme).historyItem,
-                {
-                  backgroundColor: item.materialDocumentNumber
-                    ? theme.foregroundColor
-                    : theme.secondaryForegroundColor,
-                },
-              ]}>
-              <View style={styles(theme).historyItemLeftPanel}>
-                <Text style={styles(theme).historyItemLeftPanelText}>
-                  {goodsMovementLogs.length - index}
-                </Text>
-              </View>
-
-              <View>
-                <Text style={styles(theme).historyItemHeaderContainer}>
-                  <Text style={styles(theme).historyItemHeaderText}>
-                    Ενδοδιακίνηση:{' '}
-                  </Text>
-                  <Text style={styles(theme).historyItemHeaderText2}>
-                    {item.materialDocumentNumber}
-                  </Text>
-                </Text>
-                <Text style={styles(theme).historyItemHeaderContainer2}>
-                  <Text style={styles(theme).historyItemHeaderText3}>
-                    Προέλευση:{' '}
-                  </Text>
-                  <Text style={styles(theme).historyItemHeaderText4}>
-                    {item.items[0].storageLocationIn}{' '}
-                    {storageLocations[item.items[0].storageLocationIn]}
-                  </Text>
-                </Text>
-                <Text style={styles(theme).historyItemHeaderContainer2}>
-                  <Text style={styles(theme).historyItemHeaderText3}>
-                    Προορισμός:{' '}
-                  </Text>
-                  <Text style={styles(theme).historyItemHeaderText4}>
-                    {item.items[0].storageLocationOut}{' '}
-                    {storageLocations[item.items[0].storageLocationOut]}
-                  </Text>
-                </Text>
-
-                {item.items.map((item: MaterialDocumentItem, j: number) => (
-                  //TODO: animate flatlist hiding individual items
-                  //TODO: make this view into a component GoodsMovemementLogComponent
-                  <View style={[{maxHeight: 1500}]} key={j++}>
-                    <Text style={styles(theme).historyItemLineText}>
-                      Υλικό: {item.materialNumber}
-                      {'\n'}
-                      {materialDescriptions[item.materialNumber]}
-                    </Text>
-                    <Text style={styles(theme).historyItemLineText}>
-                      Παρτίδα: {item.batch}
-                    </Text>
-                    <Text style={styles(theme).historyItemLineText}>
-                      Ποσότητα: {item.quantity}
-                      {'\n'}
+        renderItem={({item, index}) => {
+          return (
+            <Pressable
+              onPress={() => {
+                animations[index].setInterpolate(item.items.length * 90);
+              }}>
+              <View
+                style={[
+                  styles(theme).historyItem,
+                  {
+                    backgroundColor: item.materialDocumentNumber
+                      ? theme.foregroundColor
+                      : theme.secondaryForegroundColor,
+                  },
+                ]}>
+                <View style={styles(theme).historyItemTopPanel}>
+                  <View style={styles(theme).historyItemLeftPanel}>
+                    <Text style={styles(theme).historyItemLeftPanelText}>
+                      {goodsMovementLogs.length - index}
                     </Text>
                   </View>
-                ))}
+                  <View>
+                    <Text style={styles(theme).historyItemHeaderContainer}>
+                      <Text style={styles(theme).historyItemHeaderText}>
+                        Ενδοδιακίνηση:{' '}
+                      </Text>
+                      <Text style={styles(theme).historyItemHeaderText2}>
+                        {item.materialDocumentNumber}
+                      </Text>
+                    </Text>
+                    <Text style={styles(theme).historyItemHeaderContainer2}>
+                      <Text style={styles(theme).historyItemHeaderText3}>
+                        Προέλευση:{' '}
+                      </Text>
+                      <Text style={styles(theme).historyItemHeaderText4}>
+                        {item.items[0].storageLocationIn}{' '}
+                        {storageLocations[item.items[0].storageLocationIn]}
+                      </Text>
+                    </Text>
+                    <Text style={styles(theme).historyItemHeaderContainer2}>
+                      <Text style={styles(theme).historyItemHeaderText3}>
+                        Προορισμός:{' '}
+                      </Text>
+                      <Text style={styles(theme).historyItemHeaderText4}>
+                        {item.items[0].storageLocationOut}{' '}
+                        {storageLocations[item.items[0].storageLocationOut]}
+                      </Text>
+                    </Text>
+                  </View>
+                </View>
+
+                <Animated.View style={animations[index].setInterpolate()}>
+                  {item.items.map((item: MaterialDocumentItem, j: number) => {
+                    return (
+                      <View
+                        style={{
+                          marginLeft: '16%',
+                          marginTop: '1%',
+                        }}
+                        key={j++}>
+                        <Text style={styles(theme).historyItemLineText}>
+                          Υλικό: {item.materialNumber}
+                          {'\n'}
+                          {materialDescriptions[item.materialNumber]}
+                        </Text>
+                        <Text style={styles(theme).historyItemLineText}>
+                          Παρτίδα: {item.batch}
+                        </Text>
+                        <Text style={styles(theme).historyItemLineText}>
+                          Ποσότητα: {item.quantity}
+                          {'\n'}
+                        </Text>
+                      </View>
+                    );
+                  })}
+                </Animated.View>
               </View>
-            </View>
-          </Pressable>
-        )}
+            </Pressable>
+          );
+        }}
       />
     </View>
   );
 }
+
+const styles2 = StyleSheet.create({
+  hidden: {
+    height: 0,
+  },
+  list: {
+    overflow: 'hidden',
+  },
+});
 
 export default React.memo(History);
