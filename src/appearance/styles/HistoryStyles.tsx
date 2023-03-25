bcc49fcf84fce6f3015274b0883605d5a7e73484
it1@konstolymp.gr <it1@konstolymp.gr>
2023-03-25 17:34:29 +0200

    moved historyItem styles to component

--- src/appearance/styles/HistoryStyles.tsx
+++ src/appearance/styles/HistoryStyles.tsx
@@ -9,59 +9,9 @@ export const styles = (theme: any) =>
     historyHeader: {
       margin: '5%',
     },
-    historyItem: {
-      elevation: 5,
-      borderRadius: 10,
-      margin: '2%',
-    },
     historyHeaderText: {
       fontWeight: 'bold',
       fontSize: 30,
       color: theme.textColor,
-    },
-    historyItemTopPanel: {
-      flexDirection: 'row',
-    },
-    historyItemLeftPanel: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '16%',
-    },
-    historyItemLeftPanelText: {
-      color: theme.buttonTextColor,
-      fontSize: 30,
-      fontWeight: 'bold',
-    },
-    historyItemHeaderContainer: {
-      marginLeft: '4%',
-      marginTop: '2%',
-    },
-    historyItemHeaderContainer2: {
-      marginLeft: '4%',
-    },
-    historyItemHeaderText: {
-      color: theme.buttonTextColor,
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-    historyItemHeaderText2: {
-      color: theme.buttonTextColor,
-      fontSize: 18,
-    },
-    historyItemHeaderText3: {
-      color: theme.buttonTextColor,
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-    historyItemHeaderText4: {
-      color: theme.buttonTextColor,
-      fontSize: 18,
-    },
-    historyItemLineText: {
-      marginLeft: '4%',
-      color: theme.buttonTextColor,
-    },
-    historyItemMaktx: {
-      marginLeft: '4%',
     },
   });
