30b2ac0f2c14d710582c46d14d217f293272d3ab
it1@konstolymp.gr <it1@konstolymp.gr>
2023-03-26 22:50:16 +0300

    initial commit

---
+++ src/screens/settings/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Settings({navigation}: {navigation: any}): JSX.Element {
+  return (
+    <View>
+      <Text>a</Text>
+    </View>
+  );
+}
+
+export default React.memo(Settings);
