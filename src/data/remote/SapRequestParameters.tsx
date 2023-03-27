63e5eadf4086741a3badb212fa85b3aa67b9e1a8
it1@konstolymp.gr <it1@konstolymp.gr>
2023-03-27 23:24:48 +0300

    variable user credentials

--- src/data/remote/SapRequestParameters.tsx
+++ src/data/remote/SapRequestParameters.tsx
@@ -1,3 +1,4 @@
+import CookieManager from '@react-native-cookies/cookies';
 import base64 from 'react-native-base64';
 import RequestGateway, {isError} from './RequestGateway';
 
@@ -12,14 +13,37 @@ interface SapRequestHeaders {
 }
 
 class SapRequestParameters {
-  private username = 'FILKOZ';
-  private password = 'COMPO2SITION4';
+  private username: string = '';
+  private password: string = '';
 
   private CSRF_REQUEST_HEADERS = {
     'x-csrf-token': 'Fetch',
     Authorization:
       'Basic ' + base64.encode(this.username + ':' + this.password),
   };
+
+  async setSapCredentials(
+    username: string,
+    password: string,
+  ): Promise<boolean | undefined> {
+    CookieManager.clearAll();
+    this.username = username;
+    this.password = password;
+
+    this.CSRF_REQUEST_HEADERS = {
+      'x-csrf-token': 'Fetch',
+      Authorization:
+        'Basic ' + base64.encode(this.username + ':' + this.password),
+    };
+
+    const csrfToken = await this.getCSRFToken();
+
+    if (csrfToken !== undefined) {
+      return true;
+    } else {
+      return undefined;
+    }
+  }
 
   private async getCSRFToken(timeout: number = 0): Promise<string | undefined> {
     const response = await RequestGateway.get<TokenResponse>(
