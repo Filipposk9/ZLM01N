7cc9aa408c6452b6b9382e8583a591d2090f05cf
it1@konstolymp.gr <it1@konstolymp.gr>
2023-04-08 18:37:47 +0300

    storage locations are now filtered on user buildin

--- src/screens/transferposting/components/StorageLocationDropdown.tsx
+++ src/screens/transferposting/components/StorageLocationDropdown.tsx
@@ -4,6 +4,9 @@ import {SelectList} from 'react-native-dropdown-select-list';
 import {ThemeContext} from '../../../appearance/theme/ThemeContext';
 import Repository from '../../../data/Repository';
 import Icon from '../../../appearance/assets/Icon';
+import {UserState} from '../../../redux/ReduxTypes';
+import {useSelector} from 'react-redux';
+import {StorageLocation} from '../../../shared/Types';
 
 interface StorageLocationDropdownProps {
   placeholder: string;
@@ -21,12 +24,49 @@ function StorageLocationDropdown(
     selectListData: [],
   });
 
+  const user = useSelector((state: UserState) => state.user.user);
+
   useEffect(() => {
     const fetchStorageLocations = async () => {
       const storageLocations = await Repository.getStorageLocations();
 
+      let filteredStorageLocations: StorageLocation[] = [];
+
+      switch (user.buildingCode) {
+        case 'A':
+          filteredStorageLocations = storageLocations.filter(item => {
+            return (
+              item.storageLocation[0] === '1' ||
+              item.storageLocation[0] === '2' ||
+              item.storageLocation === '3100' ||
+              item.storageLocation === '3200'
+            );
+          });
+        case 'B':
+          filteredStorageLocations = storageLocations.filter(item => {
+            return (
+              item.storageLocation[0] === '1' ||
+              item.storageLocation[0] === '2' ||
+              item.storageLocation === '3100' ||
+              item.storageLocation === '3200'
+            );
+          });
+        case 'C':
+          filteredStorageLocations = storageLocations.filter(item => {
+            return (
+              item.storageLocation[0] === '3' ||
+              item.storageLocation === '2100' ||
+              item.storageLocation === '2200' ||
+              item.storageLocation === '1005' ||
+              item.storageLocation === '1100'
+            );
+          });
+        default:
+          filteredStorageLocations = storageLocations;
+      }
+
       setDropDownData({
-        selectListData: storageLocations.map(item => {
+        selectListData: filteredStorageLocations.map(item => {
           return {
             key: item.storageLocation,
             value: item.storageLocation + ' ' + item.description,
