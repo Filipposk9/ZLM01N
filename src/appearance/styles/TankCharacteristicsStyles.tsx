19a3b851e7a41298669cc16b49b2d4e8fc72d5f3
it1@konstolymp.gr <it1@konstolymp.gr>
2023-04-08 22:35:06 +0300

    styling organised

---
+++ src/appearance/styles/TankCharacteristicsStyles.tsx
@@ -0,0 +1,56 @@
+import {StyleSheet} from 'react-native';
+import {iTheme} from '../theme/types';
+
+export const styles = (theme: iTheme) =>
+  StyleSheet.create({
+    tankCharacteristicsContainer: {
+      height: '100%',
+      backgroundColor: theme.backgroundColor,
+    },
+    tankNameContainer: {
+      margin: '4%',
+    },
+    tankNameText: {
+      fontSize: 30,
+      fontWeight: 'bold',
+      color: theme.textColor,
+    },
+    characteristicsContainer: {
+      backgroundColor: theme.secondaryForegroundColor,
+      margin: '4%',
+      borderRadius: 15,
+      elevation: 5,
+    },
+    characteristicText: {
+      marginLeft: '4%',
+      color: theme.buttonTextColor,
+      fontSize: 24,
+      marginTop: '1%',
+      marginBottom: '1%',
+    },
+    topContainer: {
+      flexDirection: 'row',
+      backgroundColor: theme.foregroundColor,
+      margin: '4%',
+      borderRadius: 15,
+    },
+    tankIconContainer: {
+      justifyContent: 'center',
+      width: '47%',
+    },
+    tankInfoContainer: {
+      width: '50%',
+      backgroundColor: theme.secondaryForegroundColor,
+      borderRadius: 10,
+      elevation: 5,
+      marginTop: '4%',
+      marginBottom: '4%',
+      marginRight: '4%',
+    },
+    tankInfoText: {
+      color: theme.buttonTextColor,
+      fontSize: 18,
+      fontWeight: 'bold',
+      margin: '4%',
+    },
+  });
