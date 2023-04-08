712d6ccb2351568989cca63216f4825509e7b70f
it1@konstolymp.gr <it1@konstolymp.gr>
2023-04-09 02:13:59 +0300

    initial commit

---
+++ src/data/remote/model/TankCharacteristicsModel.tsx
@@ -0,0 +1,27 @@
+export interface TankCharacteristicsModel {
+  TANK: string;
+  MATERIALNUMBER: string;
+  MATERIALTEXT: string;
+  BATCH: string[];
+  QUANTITY: number;
+  STORAGELOCATION: string;
+  MATERIALGROUP: string;
+  EXTERNALMATERIALGROUP: string;
+  LAB: string;
+  COLOR: string;
+  QUALITY: string;
+  CROP: string;
+  UNITSPERKG: string;
+  OLIVEFLY: string;
+  GLIOSPORE: string;
+  COLORQUALITY: string;
+  HARDNESS: number;
+  ANALYSIS: string;
+  REDNESS: number;
+  PH: number;
+  SALT: number;
+}
+
+export interface TankCharacteristicsResponse {
+  data: TankCharacteristicsModel;
+}
