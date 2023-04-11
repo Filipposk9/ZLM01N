cf03f1e37958140885a044d4654a9972b869258b
it1@konstolymp.gr <it1@konstolymp.gr>
2023-04-11 22:55:43 +0300

    initial commit

---
+++ src/screens/tankmap/index.tsx
@@ -0,0 +1,34 @@
+import React, {useContext, useEffect, useState} from 'react';
+import {View, Text} from 'react-native';
+import {styles} from '../../appearance/styles/TankMapStyles';
+import {ThemeContext} from '../../appearance/theme/ThemeContext';
+import Repository from '../../data/Repository';
+import {Tank} from '../../shared/Types';
+
+function TankMap({navigation}: {navigation: any}): JSX.Element {
+  const [tanks, setTanks] = useState<Tank[]>([]);
+
+  useEffect(() => {
+    const getTankList = async () => {
+      const tankList = await Repository.getTanks();
+
+      if (tankList) {
+        setTanks(tankList);
+      }
+    };
+
+    getTankList();
+  }, []);
+
+  const {theme} = useContext(ThemeContext);
+
+  return (
+    <View style={styles(theme).tankMapContainer}>
+      <View style={styles(theme).tankContainer}>
+        <Text style={{color: 'white'}}>CB123</Text>
+      </View>
+    </View>
+  );
+}
+
+export default React.memo(TankMap);
