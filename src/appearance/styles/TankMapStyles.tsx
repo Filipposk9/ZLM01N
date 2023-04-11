acec36d9b00ba759540eae9972a1d4b303476943
it1@konstolymp.gr <it1@konstolymp.gr>
2023-04-11 22:56:06 +0300

    initial commit

---
+++ src/appearance/styles/TankMapStyles.tsx
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+import {iTheme} from '../theme/types';
+
+export const styles = (theme: iTheme) =>
+  StyleSheet.create({
+    tankMapContainer: {
+      height: '100%',
+      backgroundColor: theme.backgroundColor,
+    },
+    tankContainer: {
+      backgroundColor: theme.foregroundColor,
+      width: 50,
+      height: 50,
+      borderRadius: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
