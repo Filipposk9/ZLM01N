d94c845888768a7444f1d4d628d83fdb81c1c551
it1@konstolymp.gr <it1@konstolymp.gr>
2023-05-02 14:49:04 +0300

    finalized screen layout

--- src/screens/tankcharacteristics/index.tsx
+++ src/screens/tankcharacteristics/index.tsx
@@ -1,5 +1,13 @@
-import React, {useContext, useState} from 'react';
-import {View, ScrollView, Dimensions, Text, Alert} from 'react-native';
+import React, {useContext, useRef, useState} from 'react';
+import {
+  View,
+  ScrollView,
+  Dimensions,
+  Text,
+  Alert,
+  Pressable,
+  KeyboardAvoidingView,
+} from 'react-native';
 import Icon from '../../appearance/assets/Icon';
 import {styles} from '../../appearance/styles/TankCharacteristicsStyles';
 import {ThemeContext} from '../../appearance/theme/ThemeContext';
@@ -7,6 +15,7 @@ import {TextInput} from 'react-native-gesture-handler';
 import {iTankCharacteristics} from '../../shared/Types';
 import SapStructureValidator from '../../utilities/validators/SapStructureValidator';
 import Repository from '../../data/Repository';
+import {GlobalStyles} from '../../appearance/styles/GlobalStyles';
 
 function TankCharacteristics({navigation}: {navigation: any}): JSX.Element {
   const {theme} = useContext(ThemeContext);
@@ -14,6 +23,8 @@ function TankCharacteristics({navigation}: {navigation: any}): JSX.Element {
   const [tank, setTank] = useState<string>('');
   const [tankCharacteristicsData, setTankCharacteristicsData] =
     useState<iTankCharacteristics>();
+
+  const scrollViewRef = useRef<ScrollView>(null);
 
   const getTankCharacteristics = () => {
     const getTankCharacteristics = async () => {
@@ -78,42 +89,102 @@ function TankCharacteristics({navigation}: {navigation: any}): JSX.Element {
         </View>
       </View>
 
-      <View style={styles(theme).characteristicsContainer}>
-        <Text style={styles(theme).characteristicText}>
-          Τεμαχισμός: {tankCharacteristicsData?.unitsPerKg}
-        </Text>
+      <ScrollView style={styles(theme).characteristicsContainer}>
+        {[
+          {
+            label: 'Ποιότητα Χρώματος',
+            value: tankCharacteristicsData?.colorQuality,
+          },
+          {label: 'Σκληρότητα', value: tankCharacteristicsData?.hardness},
+          {label: 'pH', value: tankCharacteristicsData?.pH},
+          {label: 'Αλάτι', value: tankCharacteristicsData?.salt},
+          {
+            label: 'Τεμαχισμός',
+            value: tankCharacteristicsData?.unitsPerKg,
+            disabled: true,
+          },
+          {
+            label: 'Δάκος',
+            value: tankCharacteristicsData?.oliveFly,
+            disabled: true,
+          },
+          {
+            label: 'Γλοιοσπόριο',
+            value: tankCharacteristicsData?.gliospore,
+            disabled: true,
+          },
+          {
+            label: 'Ανάλυση',
+            value: tankCharacteristicsData?.analysis,
+            disabled: true,
+          },
+          {
+            label: 'Κόκκινα',
+            value: tankCharacteristicsData?.redness,
+            disabled: true,
+          },
+        ].map((characteristic, index) => (
+          <Pressable
+            key={characteristic.label}
+            style={styles(theme).characteristicLine}
+            onPress={() => scrollToElement(index)}>
+            <TextInput
+              editable={false}
+              autoCorrect={false}
+              style={styles(theme).characteristicText}>
+              {characteristic.label}
+            </TextInput>
+            <View style={styles(theme).characteristicInputContainer}>
+              <TextInput
+                keyboardType="numeric"
+                style={styles(theme).characteristicInput}
+                editable={!characteristic.disabled}>
+                {characteristic.value}
+              </TextInput>
+            </View>
+          </Pressable>
+        ))}
+      </ScrollView>
 
-        <Text style={styles(theme).characteristicText}>
-          Δάκος: {tankCharacteristicsData?.oliveFly}
-        </Text>
-
-        <Text style={styles(theme).characteristicText}>
-          Γλοιοσπόριο: {tankCharacteristicsData?.gliospore}
-        </Text>
-
-        <Text style={styles(theme).characteristicText}>
-          Ποιότητα Χρώματος: {tankCharacteristicsData?.colorQuality}
-        </Text>
-
-        <Text style={styles(theme).characteristicText}>
-          Σκληρότητα: {tankCharacteristicsData?.hardness}
-        </Text>
-
-        <Text style={styles(theme).characteristicText}>
-          Ανάλυση: {tankCharacteristicsData?.analysis}
-        </Text>
-
-        <Text style={styles(theme).characteristicText}>
-          Κόκκινα: {tankCharacteristicsData?.oliveFly}
-        </Text>
-
-        <Text style={styles(theme).characteristicText}>
-          pH: {tankCharacteristicsData?.pH}
-        </Text>
-
-        <Text style={styles(theme).characteristicText}>
-          Αλάτι: {tankCharacteristicsData?.salt}
-        </Text>
+      <View style={styles(theme).bottomContainer}>
+        <View style={styles(theme).submitButtonContainer}>
+          <Pressable
+            style={styles(theme).submitButton}
+            onPress={async () => {
+              // setIsLoading(true);
+              // const goodsMovementLog = await submitGoodsMovement(
+              //   scannedLabels,
+              //   storageLocationIn,
+              //   storageLocationOut,
+              // );
+              // setIsLoading(false);
+              // if (goodsMovementLog) {
+              //   dispatch(setGoodsMovementLog(goodsMovementLog));
+              //   navigation.navigate('TransferPostingLog', [
+              //     goodsMovementLog,
+              //     storageLocationIn,
+              //     storageLocationOut,
+              //   ]);
+              // } else {
+              //   if (
+              //     scannedLabels.length > 0 &&
+              //     storageLocationsAreValid(
+              //       storageLocationIn,
+              //       storageLocationOut,
+              //     )
+              //   ) {
+              //     resetScreenComponents();
+              //     Alert.alert(
+              //       'Ανεπαρκές Σήμα',
+              //       'Το παραστατικό προστέθηκε στην ουρά',
+              //     );
+              //   }
+              // }
+            }}
+            android_ripple={GlobalStyles(theme).rippleColor}>
+            <Text style={styles(theme).submitButtonText}>Καταχώριση</Text>
+          </Pressable>
+        </View>
       </View>
     </View>
   );
