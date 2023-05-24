ef628a350ed7d7c4f26e6d6efcebd398cf31436f
it1@konstolymp.gr <it1@konstolymp.gr>
2023-05-24 10:08:17 +0300

    using username string instead of User structure

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -84,7 +84,7 @@ function Login({navigation}: {navigation: any}): JSX.Element {
 
             if (user !== undefined) {
               dispatch(setCurrentUser(user));
-              LocationService.start(user.username);
+              LocationService.start(username);
 
               navigation.navigate('MainMenu');
             } else {
