6d9b491e03b42d4b47e41b4c96bb3bb178ced890
it1@konstolymp.gr <it1@konstolymp.gr>
2023-06-15 16:28:57 +0300

    characteristic boxes styling

--- src/appearance/styles/BarrelCharacteristicsStyles.tsx
+++ src/appearance/styles/BarrelCharacteristicsStyles.tsx
@@ -8,9 +8,66 @@ export const styles = (theme: iTheme) =>
       height: '100%',
     },
     topContainer: {
-      backgroundColor: theme.secondaryBackgroundColor,
+      backgroundColor: theme.secondaryForegroundColor,
+      borderRadius: 10,
+      margin: '2%',
     },
     headerText: {
-      color: theme.textColor,
+      color: theme.buttonTextColor,
+      marginLeft: '4%',
+      marginRight: '4%',
+      marginTop: '1%',
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    basicCharacteristicsBox: {
+      backgroundColor: theme.foregroundColor,
+      borderRadius: 10,
+      margin: '2%',
+      elevation: 25,
+    },
+    basicCharacteristicsContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    basicCharacteristicsSlider: {
+      width: '88%',
+    },
+    basicCharacteristicsText: {
+      color: theme.buttonTextColor,
+      marginLeft: '4%',
+      marginTop: '2%',
+      fontSize: 18,
+    },
+    basicCharacteristicsValue: {
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 18,
+    },
+    bottomContainer: {
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      flex: 1,
+      margin: '2%',
+    },
+    submitButtonContainer: {
+      elevation: 50,
+      borderRadius: 30,
+      borderColor: theme.borderColor,
+      backgroundColor: theme.buttonThirdBackgroundColor,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '50%',
+    },
+    submitButton: {
+      height: 60,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+    },
+    submitButtonText: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: theme.buttonTextColor,
     },
   });
