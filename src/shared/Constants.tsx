8009624e900dd905fabc9715538e8589beac1cb6
it1@konstolymp.gr <it1@konstolymp.gr>
2023-09-25 02:11:02 +0300

    added azure auth

--- src/shared/Constants.tsx
+++ src/shared/Constants.tsx
@@ -1,3 +1,5 @@
+import AzureAuth from 'react-native-azure-auth';
+
 export const GOODS_MOVEMENT_CODE = {
   GOODS_ISSUE: '03',
   TRANSFER_POSTING: '04',
@@ -17,3 +19,9 @@ export const STORAGE_LOCATION = {
   BUILDING_C_YARD: '3200',
   BUILDING_C_SCRAP: '3209',
 };
+
+export const AZURE_AUTH = new AzureAuth({
+  clientId: 'b7c14ee8-9bc0-4e9e-b32e-3f5a5e2eaeb3',
+  tenant: '1c32b97c-c0df-4764-83e5-b85512702173',
+  redirectUri: 'com.zlm01n://com.zlm01n/android/callback',
+});
