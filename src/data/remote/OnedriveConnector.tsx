cacacd1756e79ec531989a377d3083d14c2ebf4d
it1@konstolymp.gr <it1@konstolymp.gr>
2023-09-25 20:10:21 +0300

    refactored uploadImage and init connection

--- src/data/remote/OnedriveConnector.tsx
+++ src/data/remote/OnedriveConnector.tsx
@@ -1,7 +1,7 @@
 import RNFS from 'react-native-fs';
-import {Buffer} from 'buffer';
 import AzureAuth from 'react-native-azure-auth';
 import {uploadSmallFile} from '@harrisoff/onedrive-js-sdk';
+import {Buffer} from '@craftzdog/react-native-buffer';
 
 class OnedriveConnector {
   private userID: string = '';
@@ -10,7 +10,10 @@ class OnedriveConnector {
   async initializeConnection(
     azureAuth: AzureAuth,
   ): Promise<string | undefined> {
-    this.setAzureConfig(azureAuth);
+    if (this.azureAuth === undefined) {
+      this.setAzureConfig(azureAuth);
+    }
+
     const accessToken = await this.getAccessToken();
 
     //TODO: return connection info
@@ -50,45 +53,32 @@ class OnedriveConnector {
           accessToken = newTokens.accessToken;
         }
       }
-    } catch (error) {
-      console.log(error);
-    }
+    } catch (error) {}
 
     return accessToken;
   }
 
-  private async prepareImageForUpload(
-    path: string,
-  ): Promise<Buffer | undefined> {
-    const image = await RNFS.readFile(path, 'base64');
+  async uploadImage(
+    image: Buffer,
+    remotePath: string,
+  ): Promise<boolean | undefined> {
+    const accessToken = await this.getAccessToken();
 
-    const buffer = Buffer.from(image, 'base64');
+    if (accessToken && image) {
+      const onedriveResponse = await uploadSmallFile(
+        image,
+        remotePath,
+        accessToken,
+      );
 
-    if (buffer) {
-      return buffer;
-    } else {
-      return undefined;
-    }
-  }
-
-  async uploadImage(localImagePath: string, remotePath: string) {
-    const accessToken = this.getAccessToken().toString();
-
-    if (accessToken !== undefined) {
-      const image = this.prepareImageForUpload(localImagePath);
-
-      const fileName = localImagePath.split('/');
-      const fileNameWithExtension = fileName[fileName.length - 1];
-
-      if (image !== undefined) {
-        const onedriveResponse = await uploadSmallFile(
-          image,
-          remotePath + '/' + fileNameWithExtension,
-          accessToken,
-        );
-
-        // TODO: if (onedriveResponse) check the response and return true or false or the response itself
+      if (onedriveResponse) {
+        if (onedriveResponse.id !== '') {
+          return true;
+        } else {
+          return false;
+        }
       }
+      return true;
     }
   }
 }
