f5677cfc3729dbe9afec6dd5973db91ec997008c
Filippos Kozanitis <filippos.kozanitis@icloud.com>
2024-06-18 12:49:03 +0300

    Init

---
+++ src/appearance/theme/types.tsx
@@ -0,0 +1,17 @@
+export interface iTheme {
+  backgroundColor: string;
+  secondaryBackgroundColor: string;
+  foregroundColor: string;
+  secondaryForegroundColor: string;
+  buttonBackgroundColor: string;
+  buttonSecondaryBackgroundColor: string;
+  buttonThirdBackgroundColor: string;
+  borderColor: string;
+  iconColor: string;
+  buttonBorderColor: string;
+  textColor: string;
+  buttonTextColor: string;
+  secondaryTextColor: string;
+  rippleColor: string;
+  tabBorderColor: string;
+}
